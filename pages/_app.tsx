0f3253f63b97d5cb67be8c6634f2034303353ea7
Terence O'Donoghue <1697905+terenceodonoghue@users.noreply.github.com>
2021-07-11 20:59:49 +1000

    Configure Tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import React, { ReactElement } from 'react';
 import { AppProps } from 'next/app';
+import 'tailwindcss/tailwind.css';
 
 const MyApp = ({ Component, pageProps }: AppProps): ReactElement => {
   return <Component {...pageProps} />;
