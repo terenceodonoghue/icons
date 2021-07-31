c75468c3e284aa820cd9dddb6d06aaf273cb1cf2
Terence O'Donoghue <1697905+terenceodonoghue@users.noreply.github.com>
2021-07-31 20:40:25 +1000

    Create basic landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { NextPage } from 'next';
+import * as glyphs from '@terenceodonoghue/react-icons/glyphs';
 
-const HomePage: NextPage = () => <div>Welcome to Next.js!</div>;
+const HomePage: NextPage = () => (
+  <div className="flex flex-wrap">
+    {Object.entries(glyphs).map(([iconName, Icon]) => (
+      <Icon className="m-6 h-6 w-6" key={iconName} />
+    ))}
+  </div>
+);
 
 export default HomePage;
