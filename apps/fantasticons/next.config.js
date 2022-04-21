9c72f4709feff1f994f93ac3e8558dcc45e1e2a7
Terence O'Donoghue <1697905+terenceodonoghue@users.noreply.github.com>
2022-04-21 20:43:16 +1000

    Update project config

--- apps/fantasticons/next.config.js
+++ apps/fantasticons/next.config.js
@@ -1,8 +1,4 @@
-const isProd = process.env.NODE_ENV === 'production';
-
 module.exports = {
-  assetPrefix: isProd ? '/icons' : '',
-  basePath: isProd ? '/icons' : '',
   webpack: (config) => {
     config.module.rules.push({
       test: /\.m?js$/,
