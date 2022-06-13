cd63e6bfbac40133f41882855bcc9d0b45738158
Terence O'Donoghue <1697905+terenceodonoghue@users.noreply.github.com>
2022-06-13 16:46:15 +1000

    Update lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,46 +1,7 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: ['react', '@typescript-eslint'],
+  extends: ['@terenceodonoghue/react'],
   rules: {
-    '@typescript-eslint/no-use-before-define': 'warn',
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        js: 'never',
-        jsx: 'never',
-        ts: 'never',
-        tsx: 'never',
-      },
-    ],
     'import/no-unresolved': 'off', // Until eslint-plugin-import supports ESM
     'no-underscore-dangle': 'off',
-    'no-use-before-define': 'off',
-    'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
-    'react/jsx-props-no-spreading': 'off',
-  },
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
-    },
   },
 };
